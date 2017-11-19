31f81d9cdc91daabaa17ff164207d12a3cd8f33e
JeongsangBaek <hackest@gmail.com>
2017-11-19 16:11:23 +0900

    generate dummy csv  and upload to gcs

--- src/main/resources/nodejs_scripts/generate_dfp_dummy_network_request/generate_dfp_dummy_network_request.js
+++ src/main/resources/nodejs_scripts/generate_dfp_dummy_network_request/generate_dfp_dummy_network_request.js
@@ -1,13 +1,36 @@
 const co = require('co');
 const util = require('util');
 const generator = require('./generate_dfp_dummies');
+const Storage = require('@google-cloud/storage');
+const csvWriter = require('csv-write-stream');
+const fs = require('fs');
+const moment = require('moment');
 
 if (require.main === module) {
     co(async function() {
-        const genObj = generator.generateDummyByRwc();
-        util.log(`generated: ${JSON.stringify(genObj, null, 4)}`);
+        const projectId = 'dfp-data-analysis-186405';
+        const storage = new Storage({
+            projectId:projectId
+        });
+        const bucketName = 'gdfp-dummy-data-store';
+        const filename = `NetworkRequests_1_${moment().format('YYYYMMDD_HH')}.csv`;
+        const writer = csvWriter();
+        writer.pipe(fs.createWriteStream(filename));
 
+        const count = 10000 + Math.floor(Math.random() * 90000);
+        util.log(`Will be generated ${count} logs`);
+
+        for (let i = 0; i < count; i++) {
+            const genObj = generator.generateDummyByRwc();
+            // util.log(`generated: ${JSON.stringify(genObj, null, 4)}`);
+            //write json objs to csv file
+            writer.write(genObj);
+        }
+        writer.end();
+
+        await storage.bucket(bucketName).upload(filename);
+        util.log(`Finished upload ${filename} to ${bucketName}`);
     }).catch((err) => {
-        util.log(`! Exception caught: ${err}`);
+        util.log(`! Exception caught: ${err.stack}`);
     });
 }
